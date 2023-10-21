ef476711770222b4bf741414a92fcba393da3ddd
Timofey Konyshev <frastercom@gmail.com>
2023-10-21 16:32:20 +0300

    свет, дверь и тема

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void styleThem(View view) {
+        Switch sw = findViewById(R.id.styleTheme);
+        if (sw.isChecked()) {
+            Logger.getLogger("main").log(Level.ALL, "checked");
+            getApplication().setTheme(android.R.style.Theme_Holo);
+        } else {
+            Logger.getLogger("main").log(Level.ALL, "checked");
+            getApplication().setTheme(android.R.style.Theme_Black);
+        }
+    }
+    public void door(View view) {
+        Switch sw = findViewById(R.id.door);
+        if (sw.isChecked()) {
+            //дверь открыть
+        } else {
+            //дверь закрыть
+        }
+    }
+    public void light(View view) {
+        Switch sw = findViewById(R.id.light);
+        if (sw.isChecked()) {
+            //включить свет
+        } else {
+            //выключить свет
+        }
     }
 }
